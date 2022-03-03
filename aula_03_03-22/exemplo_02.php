930decca8249fad6f7eead8626b0e982627162b0
projetos-kawa <79852125+projetos-kawa@users.noreply.github.com>
2022-03-03 08:52:33 -0300

    Add files via upload

---
+++ aula_03_03-22/exemplo_02.php
@@ -0,0 +1,15 @@
+<?php
+    $soma = ($valor = 4) + 5;
+
+    $valor2 = 20;
+    $valor3 = 30;
+
+    $soma += 1;
+    $soma += $valor2;
+    $soma *= $valor3;
+    $soma %= 100;
+
+    $saudacao = "Bom";
+    $saudacao .= "Dia";
+
+    echo $saudacao . "Hoje vai ter" . $soma. "%trsiteza!";
