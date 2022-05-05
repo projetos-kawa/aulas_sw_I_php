c2e89f9360f330c010b68b565619e950092d68f4
projetos-kawa <79852125+projetos-kawa@users.noreply.github.com>
2022-05-05 09:10:59 -0300

    Add files via upload

---
+++ exercicios_lacos/exe01/calcula.php
@@ -0,0 +1,14 @@
+<?php
+    $numero = $_POST['num'];
+    //echo $numero;
+    $cubo = pow($numero, 3);
+    //echo  $cubo;
+
+    for($cont=$numero;$cont<=$cubo;$cont++){
+        echo $cont. "<br>";
+        //echo "<br>";
+    }
+
+
+
+?>
---
+++ exercicios_lacos/exe02/calcula.php
@@ -0,0 +1,21 @@
+<?php
+$numero = $_POST['num'];
+//echo $numero;
+$cubo = pow($numero, 3);
+//echo  $cubo;
+
+//Exemplo para identificar números ímpares
+//$abacaxi = 11;
+//$resto = ($abacaxi % 2);
+
+//echo $resto;
+for($cont=$numero;$cont<=$cubo;$cont++){
+    if($cont % 2 == 1){
+        echo $cont;
+        echo "<br>";
+    }
+}
+
+
+
+?>
