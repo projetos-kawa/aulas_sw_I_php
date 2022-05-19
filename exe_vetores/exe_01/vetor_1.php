370daf7b7faa31ade7b489df227321a5bc6ba98f
projetos-kawa <79852125+projetos-kawa@users.noreply.github.com>
2022-05-19 09:09:41 -0300

    Add files via upload

---
+++ exe_vetores/exe_01/vetor_1.php
@@ -0,0 +1,8 @@
+<?php
+
+$meses = ['janeiro','fevereiro','março','abril','maio','junho','julho','agosto','setembro','outubro','novembro','dezembro'];
+$numero_mes = 6;
+
+echo $meses[$numero_mes-1];
+
+?>
